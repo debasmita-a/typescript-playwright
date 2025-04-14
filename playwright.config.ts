fa1569a56616674c82f45d00092e438d8924d954
Debasmita A <72942875+debasmita-a@users.noreply.github.com>
2025-04-14 17:15:19 +0530

    github action test run

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
-    headless: false,
+    headless: true,
     trace: 'on-first-retry',
   },
   projects: [
